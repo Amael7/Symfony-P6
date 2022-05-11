75538154f5b80876eda49a8369a1503adcff060b
M.Stéphane <stephane.montoro@hotmail.com>
2022-05-11 03:07:27 +0200

    responsive medias-box with JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,35 @@
-// document.addEventListener("DOMContentLoaded", toggleDisplayMediasBox());
-document.addEventListener("onResize", function(){
-  console.log('Salut');
+// document.addEventListener : "onResize", à rajouter
+document.addEventListener("DOMContentLoaded", function() {
+  toggleDisplayMediasBox();
+
+  const mediasBoxDiv = document.getElementById('js-medias-box');
+  const btnSeeMedias = document.getElementById('js-btn-see-medias');
+
+  // When we click on the button 'see medias', we toggle the display/hide of the medias
+  btnSeeMedias.addEventListener("click", function() {
+    toggleDisplayNoneElement(mediasBoxDiv);
+  });
 });
 
+// Toggle the hiding for the medias-box div and display the button 'see medias'
 function toggleDisplayMediasBox() {
-  console.log(window.innerWidth);
+  const mediasBoxDiv = document.getElementById('js-medias-box');
+  const btnSeeMediaDiv = document.getElementById('js-div-see-medias');
 
-  const mediasBox = document.getElementsByClassName('js-medias-box')[0];
   if (window.innerWidth <= 767) {
-    console.log('salut');
-    mediasBox.classList.add('d-none');
+    mediasBoxDiv.classList.add('d-none');
+    btnSeeMediaDiv.classList.remove('d-none');
   } else {
-    console.log('salut');
+    btnSeeMediaDiv.classList.add('d-none');
+    mediasBoxDiv.classList.remove('d-none');
+  }
+}
+
+// // Check if an element has the "d-none" class and toggle 'd-none' on the element
+function toggleDisplayNoneElement(element) {
+  if (element.classList.contains('d-none')) {
+    element.classList.remove('d-none')
+  } else {
+    element.classList.add('d-none')
   }
 }
